0d724c643b9633002c378da5c9c51841c21e4da9
RafetsederTobias <43823286+RafetsederTobias@users.noreply.github.com>
2018-10-09 21:12:48 +0200

    Add files via upload
    09.10.2018_daten_einbinden

---
+++ Daten_einbinden_unterordner/app.js
@@ -0,0 +1,8 @@
+const data = require("./hallo.js");
+
+console.log(data.name("Hallo "));
+
+// require("./unterordner/app2");
+
+require("./unterordner");
+
---
+++ Daten_einbinden_unterordner/unterordner/index.js
@@ -0,0 +1,3 @@
+console.log("ich bin unterordner/index.js");
+
+require("./app2.js");
